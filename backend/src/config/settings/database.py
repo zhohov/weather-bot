b730cb11a1d124d20ddfe92bf40135d1815343fc
maksimzhohov <zhohovmaksim@gmail.com>
2024-10-10 00:39:12 +0300

    build: add docker and docker-compose for django dev

---
+++ backend/src/config/settings/database.py
@@ -0,0 +1,13 @@
+from .common import BASE_DIR
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
